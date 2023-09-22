089d5d31c78722782453081a3ba3191341c7d6ac
Akshay Negi <120248302+akshubawa@users.noreply.github.com>
2023-09-22 13:17:19 +0530

    update app.py

--- src/app.py
+++ src/app.py
@@ -44,4 +44,4 @@ def analyze():
     return render_template("result.html", result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=False)
