657b3d649d58b90ae59685caaaa74b7a983757d1
Kabir-Yadav <kabirrao1@gmail.com>
2023-09-29 13:55:41 +0530

    Hover with multiple links

--- test_part/test_part/static/index.js
+++ test_part/test_part/static/index.js
@@ -29,41 +29,78 @@
 //     popup.style.display = 'none';
 // });
 
-const hoverLink = document.querySelector('.hover-link');
+// const hoverLink = document.querySelectorAll('.hover-link');
+// const popup = document.querySelector('.popup');
+
+// const extractedText = hoverLink.textContent;
+
+// hoverLink.addEventListener('mouseenter', () => {
+//     // Display the loading message
+//     document.getElementById('extracted-text').textContent = 'Loading...';
+
+//     // Extract the text from the link
+//     const extractedText = hoverLink.textContent;
+
+//     // Send the text to the backend for processing
+//     fetch('http://127.0.0.1:5000/process-text', {
+//         method: 'POST',
+//         body: JSON.stringify({ text: extractedText }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         // Display the processed data in the popup
+//         document.getElementById('extracted-text').textContent = data.processedText;
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+
+//     // Show the popup
+//     popup.style.display = 'block';
+// });
+
+// hoverLink.addEventListener('mouseleave', () => {
+//     // Hide the popup on mouse leave
+//     popup.style.display = 'none';
+// });
+
+const hoverLinks = document.querySelectorAll('.hover-link');
 const popup = document.querySelector('.popup');
 
-const extractedText = hoverLink.textContent;
+hoverLinks.forEach((hoverLink) => {
+    hoverLink.addEventListener('mouseenter', () => {
+        // Display the loading message
+        document.getElementById('extracted-text').textContent = 'Loading...';
 
-hoverLink.addEventListener('mouseenter', () => {
-    // Display the loading message
-    document.getElementById('extracted-text').textContent = 'Loading...';
+        // Extract the text from the link
+        const extractedText = hoverLink.textContent;
 
-    // Extract the text from the link
-    const extractedText = hoverLink.textContent;
+        // Send the text to the backend for processing
+        fetch('http://127.0.0.1:5000/process-text', {
+            method: 'POST',
+            body: JSON.stringify({ text: extractedText }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Display the processed data in the popup
+            document.getElementById('extracted-text').textContent = data.processedText;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 
-    // Send the text to the backend for processing
-    fetch('http://127.0.0.1:5000/process-text', {
-        method: 'POST',
-        body: JSON.stringify({ text: extractedText }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Display the processed data in the popup
-        document.getElementById('extracted-text').textContent = data.processedText;
-    })
-    .catch(error => {
-        console.error('Error:', error);
+        // Show the popup
+        popup.style.display = 'block';
     });
 
-    // Show the popup
-    popup.style.display = 'block';
+    hoverLink.addEventListener('mouseleave', () => {
+        // Hide the popup on mouse leave
+        popup.style.display = 'none';
+    });
 });
-
-hoverLink.addEventListener('mouseleave', () => {
-    // Hide the popup on mouse leave
-    popup.style.display = 'none';
-});
-
